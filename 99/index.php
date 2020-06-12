c0f9acf5f2e4c5d500fe9f54ce9e1955f91763be
daiko8888t <66841197+daiko8888t@users.noreply.github.com>
2020-06-13 02:15:36 +0900

    Add files via upload

---
+++ 99/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>99</title>
+  <style>
+  body {
+    font-family: "Hiragino Kaku Gothic Pro N",
+    Meiryo,sans-serif;
+  }
+  table {
+    border-collapse;
+    boder: 1px solid coral;
+  }
+  th, td {
+    width: 50px;
+    border: 1px dotted coral;
+}
+th {
+    background: lightpink;
+}
+td {
+    text-align: center;
+}
+th:hover, td:hover{
+    background: #4169e1;
+}
+.color {
+    background: #ffff00;
+}
+  </style>
+
+</head>
+<body>
+
+
+<?php 
+
+
+?>
+
+
+</table>
+  
+</body>
+</html>
